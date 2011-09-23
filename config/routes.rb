6f790ae9d96c5982c04241be675d44a1ec952ebc
Stephen Mizell <smizell@gmail.com>
2011-09-23 15:30:27 -0500

    Added user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
+
   get "sessions/new"
 
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
